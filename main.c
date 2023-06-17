09047714d95331862c877133652bb8018ac3083c
Ali Salehi <nysalehi@gmail.com>
2023-06-17 11:58:29 +0200

    Add sample spi slave code.

--- main.c
+++ main.c
@@ -1,17 +1,51 @@
-#include "pico/stdlib.h"
+#include <stdio.h>
+#include <string.h>
 
-int main()
+#include <pico/stdlib.h>
+#include <pico/binary_info.h>
+#include <hardware/spi.h>
+
+#define BUF_LEN 256
+uint8_t tx_buf[BUF_LEN] = {}, rx_buf[BUF_LEN] = {};
+size_t tx_buf_idx = 0, rx_buf_idx = 0;
+
+int main() 
 {
-    const uint LED_PIN = 25;
+    stdio_init_all();
+    
+    gpio_init(MAIN_BOARD_LED_PIN);
+    gpio_set_dir(MAIN_BOARD_LED_PIN, GPIO_OUT);
+    gpio_put(MAIN_BOARD_LED_PIN, 1-gpio_get_out_level(MAIN_BOARD_LED_PIN));
+    
+    spi_init(spi_default, 4 * 1000 * 1000);
+    spi_set_slave(spi_default, true);
+    spi_set_format(spi_default, 8, 1, 1, SPI_MSB_FIRST);
+    gpio_set_function(MAIN_BOARD_SPI_RX_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(MAIN_BOARD_SPI_TX_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(MAIN_BOARD_SPI_SCK_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(MAIN_BOARD_SPI_CSN_PIN, GPIO_FUNC_SPI);
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    bi_decl(bi_4pins_with_func(
+        MAIN_BOARD_SPI_RX_PIN,
+        MAIN_BOARD_SPI_TX_PIN,
+        MAIN_BOARD_SPI_SCK_PIN,
+        MAIN_BOARD_SPI_CSN_PIN,
+        GPIO_FUNC_SPI));
 
-    while (true) 
+    for (size_t i = 0; i < BUF_LEN; ++i)
     {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(500);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(500);
+        tx_buf[i] = ~i;
+    }
+
+    for (size_t i = 0; ; ++i)
+    {
+        // Write the output buffer to MISO, and at the same time read from MOSI.
+        spi_write_read_blocking(spi_default, tx_buf, rx_buf, BUF_LEN);
+
+        // Write to stdio whatever came in on the MOSI line.
+        //printf("SPI slave says: read page %d from the MOSI line:\n", i);
+        //printbuf(in_buf, BUF_LEN);
+
+        gpio_put(MAIN_BOARD_LED_PIN, 1-gpio_get_out_level(MAIN_BOARD_LED_PIN));
     }
 }
